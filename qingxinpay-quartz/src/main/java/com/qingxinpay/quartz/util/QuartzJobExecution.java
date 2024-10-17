843155ccc45788d72b0a3d70f24a82d61d8fa309
wangweijie <1102826054@qq.com>
2024-10-17 11:48:47 +0800

    init

---
+++ qingxinpay-quartz/src/main/java/com/qingxinpay/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.qingxinpay.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import com.qingxinpay.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
